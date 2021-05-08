446f92b3b4900bbd88da5562a3e6e0d4a4c47f9d
Daniel Bast <2790401+dbast@users.noreply.github.com>
2021-05-08 14:01:15 +0200

    Update to 3.000

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/fonts || true
-install -v -m644 ./ofl/inconsolata/Inconsolata-Regular.ttf ${PREFIX}/fonts/Inconsolata-Regular.ttf
-install -v -m644 ./ofl/inconsolata/Inconsolata-Bold.ttf ${PREFIX}/fonts/Inconsolata-Bold.ttf
+install -v -m644 ./ttf/Inconsolata-Regular.ttf ${PREFIX}/fonts/Inconsolata-Regular.ttf
+install -v -m644 ./ttf/Inconsolata-Bold.ttf ${PREFIX}/fonts/Inconsolata-Bold.ttf
