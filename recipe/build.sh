47dce4a665e85467e8793acc2998d6f4885d2488
Daniel Bast <2790401+dbast@users.noreply.github.com>
2021-05-08 15:10:19 +0200

    Use full archive that contains license

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/fonts || true
-install -v -m644 ./ttf/Inconsolata-Regular.ttf ${PREFIX}/fonts/Inconsolata-Regular.ttf
-install -v -m644 ./ttf/Inconsolata-Bold.ttf ${PREFIX}/fonts/Inconsolata-Bold.ttf
+install -v -m644 ./fonts/ttf/Inconsolata-Regular.ttf ${PREFIX}/fonts/Inconsolata-Regular.ttf
+install -v -m644 ./fonts/ttf/Inconsolata-Bold.ttf ${PREFIX}/fonts/Inconsolata-Bold.ttf
